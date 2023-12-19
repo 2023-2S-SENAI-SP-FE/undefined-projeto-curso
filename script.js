ccbd61a85b3b58b5aa0f2adeb54bbcadda2ee3b9
miguelsouz <142321080+miguelsouz@users.noreply.github.com>
2023-12-19 12:42:04 +0000

    feat: processo de style do painel

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
+let speed = parseInt(document.getElementById("speed").innerHTML);
+let desacelerando = false;
 
-// Função para verificar se o pedal de freio está pressionado
-function verificarFreio() {
-    // Suponha que você tenha um input no seu HTML com o id "pedalFreio"
-    var pedalFreio = document.getElementById("pedalFreio");
+document.addEventListener("keydown", (event) => {
+  // acelera com a tecla "a"
+  if (event.key === "w") {
+    document.getElementById("speed").innerHTML = ++speed;
+  }
 
-    // Verifica se o pedal de freio está pressionado
-    if (pedalFreio.checked) {
-        alert("Freio acionado. O sistema de freios está ativo!");
-    } else {
-        alert("Freio não acionado. Empurre para ativar o sistema de freios!");
+  // freia com a tecla "b"
+  if (event.key === "s") {
+    if (speed > 0) {
+      document.getElementById("speed").innerHTML = --speed;
     }
+  }
+});
+
+// inicia perda de velocidade após a tecla "a" ser liberada
+document.addEventListener("keyup", (event) => {
+  if (event.key === "w" && !desacelerando) {
+    perdeVelocidade();
+
+    desacelerando = true;
+  }
+});
+
+//perde velocidade até 0
+function perdeVelocidade() {
+  if (speed > 0) {
+    document.getElementById("speed").innerHTML = --speed;
+
+    setTimeout(() => {
+      perdeVelocidade();
+    }, 300);
+  } else {
+    desacelerando = false;
+  }
 }
--- testePisca.js
+++
@@ -1,9 +0,0 @@
-
-var piscando = document.getElementById('pisca');
-var interval = window.setInterval(function(){
-    if(piscando.style.visibility == 'hidden'){
-        piscando.style.visibility = 'visible';
-    }else{
-        piscando.style.visibility = 'hidden';
-    }
-}, 700);
