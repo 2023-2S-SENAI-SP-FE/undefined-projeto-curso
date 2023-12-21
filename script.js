ced834dc2be9c035c4dd79c1f68bdcf848a6c4ee
miguelsouz <142321080+miguelsouz@users.noreply.github.com>
2023-12-21 13:59:01 +0000

    feat: error

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
-function data() {
-    const data = new Date().toLocaleString();
-    return data
+let speed = parseInt(document.getElementById("speed").innerHTML);
+let desacelerando = false;
+
+document.addEventListener("keydown", (event) => {
+  // acelera com a tecla "a"
+  if (event.key === "w") {
+    document.getElementById("speed").innerHTML = ++speed;
+  }
+
+  // freia com a tecla "b"
+  if (event.key === "s") {
+    if (speed > 0) {
+      document.getElementById("speed").innerHTML = --speed;
+    }
+  }
+});
+
+// inicia perda de velocidade após a tecla "a" ser liberada
+document.addEventListener("keyup", (event) => {
+  if (event.key === "w" && !desacelerando) {
+    perdeVelocidade();
+
+    desacelerando = true;
+  }
+});
+
+//perde velocidade até 0
+function perdeVelocidade() {
+  if (speed > 0) {
+    document.getElementById("speed").innerHTML = --speed;
+
+    setTimeout(() => {
+      perdeVelocidade();
+    }, 300);
+  } else {
+    desacelerando = false;
+  }
 }
-do {
-    opcao =parseInt(prompt('Relogio Painel'))
-    console.log('data e hora : ' + data)
-    console.log('Repetição');
