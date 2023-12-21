f7f50010c263de3aaeb6359dd911c134a39a2b1c
miguelsouz <142321080+miguelsouz@users.noreply.github.com>
2023-12-21 14:42:45 +0000

    feat: tentando resolver velo e relo

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ let speed = parseInt(document.getElementById("speed").innerHTML);
 let desacelerando = false;
 
 document.addEventListener("keydown", (event) => {
-  // acelera com a tecla "a"
+  // acelera com a tecla "w"
   if (event.key === "w") {
     document.getElementById("speed").innerHTML = ++speed;
   }
 
-  // freia com a tecla "b"
+  // freia com a tecla "s"
   if (event.key === "s") {
     if (speed > 0) {
       document.getElementById("speed").innerHTML = --speed;
@@ -15,7 +15,7 @@ document.addEventListener("keydown", (event) => {
   }
 });
 
-// inicia perda de velocidade após a tecla "a" ser liberada
+// inicia perda de velocidade após a tecla "w" ser liberada
 document.addEventListener("keyup", (event) => {
   if (event.key === "w" && !desacelerando) {
     perdeVelocidade();
@@ -36,3 +36,24 @@ function perdeVelocidade() {
     desacelerando = false;
   }
 }
+
+function updateClock() {
+  var now = new Date();
+  var hours = now.getHours();
+  var minutes = now.getMinutes();
+  var seconds = now.getSeconds();
+
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
+  var timeString = hours + ":" + minutes + ":" + seconds;
+
+  document.getElementById("clock").innerText = timeString;
+}
+
+// Atualizar o relógio a cada segundo
+setInterval(updateClock, 1000);
+
+// Inicializar o relógio quando a página carrega
+updateClock();
