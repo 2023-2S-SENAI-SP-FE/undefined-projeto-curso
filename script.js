58be51eb93422b6e900bd195dcf9abd0609a1354
BarbaraUchoa <barbarahelenauchoa@gmail.com>
2024-01-05 14:01:50 +0000

    feat: iniciando a estrutura do projeto

--- script.js
+++
@@ -1,8 +0,0 @@
-function data() {
-    const data = new Date().toLocaleString();
-    return data
-}
-do {
-    opcao =parseInt(prompt('Relogio Painel'))
-    console.log('data e hora : ' + data)
-    console.log('Repetição');
