baaf2b69d1a9f893d362fda7fa565709c4ce93e4
JoaoRoccella <roccella@gmail.com>
2024-01-05 08:58:44 -0300

    fix: código script com erro

--- script.js
+++ script.js
@@ -2,7 +2,3 @@ function data() {
     const data = new Date().toLocaleString();
     return data
 }
-do {
-    opcao =parseInt(prompt('Relogio Painel'))
-    console.log('data e hora : ' + data)
-    console.log('Repetição');
