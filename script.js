66ce21806af228e365dd5b7aab5e63ffb4629fb2
BarbaraUchoa <barbarahelenauchoa@gmail.com>
2024-01-05 14:41:12 +0000

    feat:Ajustando relogio

--- script.js
+++ script.js
@@ -12,3 +12,22 @@ function data() {
     const data = new Date().toLocaleString();
     return data
 }
+function updateClock() {
+      var now = new Date();
+      var hours = now.getHours();
+      var minutes = now.getMinutes();
+      var seconds = now.getSeconds();
+
+      hours = hours < 10 ? "0" + hours : hours;
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+
+      var timeString = hours + ":" + minutes + ":" + seconds;
+
+      document.getElementById("clock").innerText = timeString;
+}
+
+    setInterval(updateClock, 1000);
+    updateClock();
+
+
