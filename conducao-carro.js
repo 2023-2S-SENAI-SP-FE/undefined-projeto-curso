65b8d1ae238136e6ea0edfac527a643ac90f7c89
Miguelallan <miguel2020allan@gmail.com>
2024-01-08 14:49:58 +0000

    fix: delimitando o velocimetro

--- conducao-carro.js
+++ conducao-carro.js
@@ -1,33 +1,36 @@
+
 let speed = parseInt(document.getElementById("speed").innerHTML);
 let desacelerando = false;
 
-  document.addEventListener("keydown", (event) => {
-      // acelera com a tecla "w"
-    if (event.key === "w") {
+document.addEventListener("keydown", (event) => {
+
+  if (event.key === "w") {
+    if (speed < 230) {
       document.getElementById("speed").innerHTML = ++speed;
     }
-    
-    // freia com a tecla "s"
-    if (event.key === "s") {
-      if (speed > 0) {
-        document.getElementById("speed").innerHTML = --speed;
-      }
+  }
+ 
+ 
+ if (event.key === "s") {
+    if (speed > 0) {
+      document.getElementById("speed").innerHTML = --speed;
     }
-  });
-
-
-// inicia perda de velocidade após a tecla "w" ser liberada
-document.addEventListener("keyup", (event) => {
-  if (event.key === "w" && !desacelerando) {
-    perdeVelocidade();
-
-    desacelerando = true;
   }
 });
 
-//perde velocidade até 0
+
+document.addEventListener("keyup", (event) => {
+ if (event.key === "w" && !desacelerando) {
+    perdeVelocidade();
+    desacelerando = true;
+
+  }
+
+});
+
+
 function perdeVelocidade() {
-  if (speed > 0) {
+ if (speed > 0) {
     document.getElementById("speed").innerHTML = --speed;
 
     setTimeout(() => {
@@ -37,4 +40,3 @@ function perdeVelocidade() {
     desacelerando = false;
   }
 }
-
