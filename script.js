22a16a5a93c0267e899b3901bb5ff4a9b587d0ee
Miguelallan <miguel2020allan@gmail.com>
2024-01-10 14:43:16 +0000

    fix: fazendo o som do carro para multimídia

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
-let speed = parseInt(document.getElementById("speed").innerHTML);
-let desacelerando = false;
+// let speed = parseInt(document.getElementById("speed").innerHTML);
+// let desacelerando = false;
 
-document.addEventListener("keydown", (event) => {
-  // acelera com a tecla "a"
-  if (event.key === "w") {
-    document.getElementById("speed").innerHTML = ++speed;
-  }
+// document.addEventListener("keydown", (event) => {
+//   // acelera com a tecla "a"
+//   if (event.key === "w") {
+//     document.getElementById("speed").innerHTML = ++speed;
+//   }
 
-  // freia com a tecla "b"
-  if (event.key === "s") {
-    if (speed > 0) {
-      document.getElementById("speed").innerHTML = --speed;
-    }
-  }
-});
+//   // freia com a tecla "b"
+//   if (event.key === "s") {
+//     if (speed > 0) {
+//       document.getElementById("speed").innerHTML = --speed;
+//     }
+//   }
+// });
 
-// inicia perda de velocidade após a tecla "a" ser liberada
-document.addEventListener("keyup", (event) => {
-  if (event.key === "w" && !desacelerando) {
-    perdeVelocidade();
+// // inicia perda de velocidade após a tecla "a" ser liberada
+// document.addEventListener("keyup", (event) => {
+//   if (event.key === "w" && !desacelerando) {
+//     perdeVelocidade();
 
-    desacelerando = true;
-  }
-});
+//     desacelerando = true;
+//   }
+// });
 
-//perde velocidade até 0
-function perdeVelocidade() {
-  if (speed > 0) {
-    document.getElementById("speed").innerHTML = --speed;
+// //perde velocidade até 0
+// function perdeVelocidade() {
+//   if (speed > 0) {
+//     document.getElementById("speed").innerHTML = --speed;
 
-    setTimeout(() => {
-      perdeVelocidade();
-    }, 300);
-  } else {
-    desacelerando = false;
-  }
-}
+//     setTimeout(() => {
+//       perdeVelocidade();
+//     }, 300);
+//   } else {
+//     desacelerando = false;
+//   }
+// }
