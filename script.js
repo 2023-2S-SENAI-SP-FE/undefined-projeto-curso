d683b312966391746506a39b675351a4c240a83a
Miguelallan <miguel2020allan@gmail.com>
2024-01-17 13:24:17 +0000

    fix: recondicionando o ar-condicionado

--- script.js
+++ script.js
@@ -9,3 +9,45 @@ function openModal(){
         }
     })
 }
+
+function updateProgressBar() {
+    var progressBar = document.getElementById('progress-fill');
+    var temperatura = parseInt(document.getElementById('temperatura').innerText);
+
+    
+    var targetPercentage = (temperatura - 10) * 5; 
+
+    
+    var fillWidth = targetPercentage / 100 * progressBar.parentElement.clientWidth;
+
+    progressBar.style.width = fillWidth + 'px';
+
+    
+    if (fillWidth >= progressBar.parentElement.clientWidth / 2) {
+      progressBar.style.backgroundColor = '#FFA500'; 
+    }
+  }
+
+  function diminuirTemperatura() {
+    var temperaturaElement = document.getElementById('temperatura');
+    var temperatura = parseInt(temperaturaElement.innerText);
+
+    if (temperatura > 10) {
+      temperatura--;
+      temperaturaElement.innerText = temperatura;
+      updateProgressBar();
+    }
+  }
+
+  function aumentarTemperatura() {
+    var temperaturaElement = document.getElementById('temperatura');
+    var temperatura = parseInt(temperaturaElement.innerText);
+
+    if (temperatura < 30) {
+      temperatura++;
+      temperaturaElement.innerText = temperatura;
+      updateProgressBar();
+    }
+  }
+
+  updateProgressBar();
