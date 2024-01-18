fd5c08288b1cecce95591fd09ebab3e8be1aa6eb
Joao Roccella <roccella@gmail.com>
2024-01-18 01:30:21 +0000

    style: adicionando ícones

--- script.js
+++ script.js
@@ -6,7 +6,13 @@ function dialNumber(number) {
 
 function call() {
     phoneScreen.innerHTML = "Calling...<br>";
+    phoneScreen.classList.add('phone-digitos')
     setTimeout(function() {
         phoneScreen.innerHTML += "Connected!";
     }, 2000);
 }
+
+// limpa o que estiver digitado no visor
+document.querySelector('#phoneScreenReset').addEventListener('click', () => {
+    phoneScreen.innerHTML = '';
+})
