59bf401ed4e69898b6ed8b1f63e99395aafc429f
Joao Roccella <roccella@gmail.com>
2024-01-18 01:38:06 +0000

    feat: desligar chamada

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 let phoneScreen = document.getElementById('phone-screen');
 
 function dialNumber(number) {
+    phoneScreen.classList.remove('phone-status');
     phoneScreen.innerHTML += number;
 }
 
+
+// realiza uma chamada
 function call() {
-    phoneScreen.innerHTML = "Calling...<br>";
-    phoneScreen.classList.add('phone-digitos')
+    phoneScreen.innerHTML = 'Ligando...<br>';
+    phoneScreen.classList.add('phone-status')
     setTimeout(function() {
-        phoneScreen.innerHTML += "Connected!";
+        phoneScreen.innerHTML = 'Conectado';
     }, 2000);
+}
+
+// encerra a chamada ativa 
+function endCall() {
+    phoneScreen.innerHTML = 'Encerrando...';
+    setTimeout(() => {
+        phoneScreen.innerHTML = '';
+    }, 1000);
 }
 
 // limpa o que estiver digitado no visor
