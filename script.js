6a0837a4e110ee8448e75e2e682414763cb2606f
Joao Roccella <roccella@gmail.com>
2024-01-18 01:55:41 +0000

    feat: funcionalidades de ligação e encerramento

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 let phoneScreen = document.getElementById('phone-screen');
+let conectado = false;
 
 function dialNumber(number) {
-    phoneScreen.classList.remove('phone-status');
-    phoneScreen.innerHTML += number;
+    if (!conectado) {
+        phoneScreen.classList.remove('phone-status');
+        phoneScreen.innerHTML += number;
+    }
 }
-
 
 // realiza uma chamada
 function call() {
@@ -13,17 +15,29 @@ function call() {
     setTimeout(function() {
         phoneScreen.innerHTML = 'Conectado';
     }, 2000);
+    conectado = true;
 }
 
 // encerra a chamada ativa 
 function endCall() {
-    phoneScreen.innerHTML = 'Encerrando...';
-    setTimeout(() => {
-        phoneScreen.innerHTML = '';
-    }, 1000);
+    if (conectado) {
+        phoneScreen.innerHTML = 'Encerrando...';
+        setTimeout(() => {
+            phoneScreen.innerHTML = '';
+        }, 1000);
+        conectado = !conectado;
+    }
 }
+
+// alterna o mute do microfone
+document.querySelector('#toggleMicrofone').addEventListener('click', e => {
+    e.target.querySelector('i').classList.toggle('bi-mic-fill');
+    e.target.querySelector('i').classList.toggle('bi-mic-mute-fill');
+})
 
 // limpa o que estiver digitado no visor
 document.querySelector('#phoneScreenReset').addEventListener('click', () => {
-    phoneScreen.innerHTML = '';
+    if (!conectado) {
+        phoneScreen.innerHTML = '';
+    }
 })
