90e0a43ce9fe259558e43f26f59d1fb41d98703d
Miguelallan <miguel2020allan@gmail.com>
2024-01-19 11:59:54 +0000

    fix: adicionando video-loading

---
+++ controle-video.js
@@ -0,0 +1,15 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    const video = document.querySelector('#videoFord');
+  
+    video.addEventListener('ended', function () {
+     
+      hideLoadingVideo();
+    });
+  
+    function hideLoadingVideo() {
+      const loadingVideoContainer = document.querySelector('.loading-video-container');
+      loadingVideoContainer.style.display = 'none';
+    }
+  
+});
